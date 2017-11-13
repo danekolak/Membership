d791edb4129d03294e8fc2b9982d36e9cedf4f7f
Danijel Kolak <danijel.kolak123@hotmail.com>
2017-11-13 09:10:48 +0100

    fgdgdgfdg

--- Memberships/Controllers/HomeController.cs
+++ Memberships/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Memberships.Controllers
 {
@@ -19,7 +15,7 @@ namespace Memberships.Controllers
 
             return View();
         }
-
+        //sdfsdff
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
